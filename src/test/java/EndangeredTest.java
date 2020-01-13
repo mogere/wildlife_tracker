fc7fcfe08fb86d629a29d6ee383bec2c69e2bb6e
mogere <pmnyatichi@gmail.com>
2020-01-13 10:52:33 +0300

    add postgres dependency to build.gradle

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
+
+    @Test
+    public void animals_instanciatesCorrectly_true() {
+        Endangered endangered = new Endangered("Lion");
+        assertEquals(true, endangered instanceof Endangered);
+
+    }
 
 }
